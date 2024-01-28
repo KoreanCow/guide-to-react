8104777cbfe8f45ae7f37eb6b519843d70c51176
KoreanCow <>
2024-01-28 16:11:45 +0900

    react node api communication

---
+++ axios-back/routes/question.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const testData = [{
+  id: 1,
+  text: 'test 1'
+}, {
+  id: 2,
+  text: 'test 2'
+}, {
+  id: 3,
+  text: 'test #3'
+}, {
+  id: 4,
+  text: 'test 4'
+}, {
+  id: 5,
+  text: 'test 5'
+},]
+router.get('/', function (req, res, next) {
+  res.json(testData);
+});
+
+module.exports = router;
---
+++ axios-back/routes/result.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  const test = req.body;
+
+  // 클라이언트로 test 변수를 응답으로 보냅니다.
+  res.json(test); // 여기서 응답을 보내고 종료합니다.
+});
+
+module.exports = router;
