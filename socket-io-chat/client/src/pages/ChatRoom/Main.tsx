8f415aaf5707752388f26794c6cc3781142ecb5e
KoreanCow <vue-cli@kakao.com>
2024-04-05 12:33:50 +0900

    Feat: Client Server Socket connection

--- socket-io-chat/client/src/pages/ChatRoom/Main.tsx
+++ socket-io-chat/client/src/pages/ChatRoom/Main.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { io } from 'socket.io-client'
+
 
 const Main = () => {
+  useEffect(() => {
+    const socket = io(process.env.REACT_APP_SERVER_URL || '', { withCredentials: true });
+
+    socket.on('connect', () => {
+      console.log('connected to server')
+    })
+
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
   return (
     <div className=' w-70 h-7 bg-cyan-300'>
       asdasdads
--- socket-io-chat/server/app.js
+++ socket-io-chat/server/app.js
@@ -1,25 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io')
+require('dotenv').config();
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT;
+const server = http.createServer(app);
+// const io = socketio(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true // 필요에 따라 설정
+  }
+});
 
-var app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
+io.on('connection', socket => {
+  console.log('New WS connection...');
+})
+
+// ---------------------------------------
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -36,4 +52,27 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+server.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+})
+
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('joinRoom', (room) => {
+//     if (chatRooms.includes(room)) {
+//       socket.join(room);
+//       console.log(`User joined room ${room}`);
+//     } else {
+//       console.log(`Room ${room} does not exist`);
+//     }
+//   });
+
+//   socket.on('chatMessage', (data) => {
+//     io.to(data.room).emit('message', data.message);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
