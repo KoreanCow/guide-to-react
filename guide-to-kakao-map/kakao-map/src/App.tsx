41602e86699a1c889d3faf42c0a8925173a26dc1
KoreanCow <vue-cli@kakao.com>
2024-05-02 18:55:16 +0900

    feat: using react-kakao-maps-sdk lib

--- guide-to-kakao-map/kakao-map/src/App.tsx
+++ guide-to-kakao-map/kakao-map/src/App.tsx
@@ -4,12 +4,20 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import NoSdk from './page/NoSdk';
 import Main from './page/Main';
+import UseSdk from './page/UseSdk';
+import { useKakaoLoader } from 'react-kakao-maps-sdk';
 
 function App() {
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.REACT_APP_KAKAO_KEY || '',
+  })
+  console.log(process.env.REACT_APP_KAKAO_KEY)
+
   return (
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/nosdk' element={<NoSdk />} />
+      <Route path='/usesdk' element={<UseSdk />} />
     </Routes>
   )
 }
--- guide-to-kakao-map/kakao-map/src/page/UseSdk.tsx
+++ guide-to-kakao-map/kakao-map/src/page/UseSdk.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const UseSdk = () => {
-  return (
-    <div>
-      using kakao maps sdk lib
-    </div>
-  )
-}
+  const [latitude, setLatitude] = useState<number>(33);
+  const [longitude, setLongitude] = useState<number>(31);
+  const [level, setLevel] = useState(3);
 
-export default UseSdk
+  const mapRef = useRef<kakao.maps.Map>(null);
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition, showError);
+      } else {
+        alert('브라우저에서 Geolocation이 지원되지 않습니다.');
+      }
+    };
+
+    const showPosition = (position: GeolocationPosition) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    };
+
+    const showError = (error: GeolocationPositionError) => {
+      alert('Geolocation 오류: ' + error.message);
+    };
+
+    getLocation();
+  }, []);
+
+  const handleLevel = (type: 'increase' | 'decrease') => {
+    const map = mapRef.current;
+    if (!map) return;
+
+    if (type === 'increase') {
+      map.setLevel(map.getLevel() + 1);
+      setLevel(map.getLevel());
+    } else if (type === 'decrease') {
+      map.setLevel(map.getLevel() - 1);
+      setLevel(map.getLevel());
+    }
+  };
+
+  return (
+    <div className="App">
+      <Map
+        id="map"
+        center={{
+          lat: latitude,
+          lng: longitude,
+        }}
+        zoomable={true}
+        ref={mapRef}
+        level={level}
+      >
+        <MapMarker
+          position={{
+            lat: latitude,
+            lng: longitude,
+          }}
+        />
+      </Map>
+      <button onClick={() => handleLevel('decrease')}>Zoom In</button>
+      <button onClick={() => handleLevel('increase')}>Zoom Out</button>
+    </div>
+  );
+};
+
+export default UseSdk;
