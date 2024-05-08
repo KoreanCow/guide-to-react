2860ae6b01e7f1b8935a8c154101ea3de178267d
KoreanCow <vue-cli@kakao.com>
2024-05-08 18:52:40 +0900

    feat: create SphereComponent

--- react-3d/react-three-fiber-1/src/App.tsx
+++ react-3d/react-three-fiber-1/src/App.tsx
@@ -1,28 +1,62 @@
 import { OrbitControls, Stars, Sphere } from '@react-three/drei';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import React, { useRef } from 'react';
 import * as THREE from 'three';
-import './App.css'
+import './App.css';
 import { Mesh } from 'three';
 
 function TestSphere() {
   const meshRef = useRef<Mesh | null>(null);
+  const texture = useLoader(THREE.TextureLoader, '/public/flower.svg');
+
+  texture.repeat.set(2, 2);
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
 
   useFrame(() => {
     if (meshRef.current) {
-      meshRef.current.rotation.x += 0.001;
+      meshRef.current.rotation.x += 0.003;
       meshRef.current.rotation.y += 0.001;
-
     }
-  })
+  });
+
   return (
-    <mesh ref={meshRef}>
-      <Sphere args={[1, 32, 32]} position={[0, 0, 0]} />
-    </mesh>
-  )
+    <group position={[2, 0, 0]}>
+      <mesh ref={meshRef}>
+        <Sphere args={[1, 32, 32]} position={[0, 0, 0]} >
+          <meshBasicMaterial attach='material' map={texture} />
+        </Sphere>
+      </mesh>
+    </group>
+  );
 }
 
+
+function SphereComponent({ positions }: { positions: [number, number, number] }) {
+  const meshRef = useRef<Mesh | null>(null);
+  const texture = useLoader(THREE.TextureLoader, '/public/flower.svg');
+
+  texture.repeat.set(2, 2);
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.003;
+      meshRef.current.rotation.y += 0.001;
+    }
+  });
+
+  return (
+    <group position={positions}>
+      <mesh ref={meshRef}>
+        <Sphere args={[1, 32, 32]} position={[0, 0, 0]} >
+          <meshBasicMaterial attach='material' map={texture} />
+        </Sphere>
+      </mesh>
+    </group>
+  );
+}
 function BackgroundStars() {
   const sceneRef = useRef<Mesh | null>(null);
 
@@ -31,23 +65,24 @@ function BackgroundStars() {
       sceneRef.current.rotation.x += 0.001;
       sceneRef.current.rotation.y += 0.001;
     }
-  })
+  });
+
   return (
     <mesh ref={sceneRef}>
       <Stars />
     </mesh>
-  )
+  );
 }
-function App() {
 
+function App() {
   return (
     <div className='body'>
       <div className='container'>
         <Canvas>
           <OrbitControls />
           <ambientLight intensity={0.5} />
-          {/* <Stars /> */}
           <BackgroundStars />
+          <SphereComponent positions={[4, 0, 0]} />
           <TestSphere />
 
         </Canvas>
