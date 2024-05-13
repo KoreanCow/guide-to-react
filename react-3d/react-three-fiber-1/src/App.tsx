304a0f3fa0ede5889f5fde101839d23a7aa0b164
KoreanCow <vue-cli@kakao.com>
2024-05-13 11:20:39 +0900

    feat: Sphere image add

--- react-3d/react-three-fiber-1/src/App.tsx
+++ react-3d/react-three-fiber-1/src/App.tsx
@@ -1,49 +1,13 @@
-import { OrbitControls, Stars, Dodecahedron, Sphere } from '@react-three/drei';
+import { OrbitControls, Stars, Sphere } from '@react-three/drei';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import * as THREE from 'three';
 import './App.css';
 import { Mesh } from 'three';
 
-
-function TestDode() {
-  // const texture = useLoader(THREE.TextureLoader, '/public/flower.svg');
-  return (
-    <mesh>
-      <Dodecahedron args={[1, 5]} />
-    </mesh>
-  )
-}
-function TestSphere() {
-  const meshRef = useRef<Mesh | null>(null);
-  const texture = useLoader(THREE.TextureLoader, '/public/flower.svg');
-
-  texture.repeat.set(2, 2);
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.003;
-      meshRef.current.rotation.y += 0.001;
-    }
-  });
-
-  return (
-    <group position={[2, 0, 0]}>
-      <mesh ref={meshRef}>
-        <Sphere args={[1, 32, 32]} position={[0, 0, 0]} >
-          <meshBasicMaterial attach='material' map={texture} />
-        </Sphere>
-      </mesh>
-    </group>
-  );
-}
-
-
 function SphereComponent({ positions }: { positions: [number, number, number] }) {
   const meshRef = useRef<Mesh | null>(null);
-  const texture = useLoader(THREE.TextureLoader, '/public/flower.svg');
+  const texture = useLoader(THREE.TextureLoader, '/public/test.jpeg');
 
   texture.repeat.set(2, 2);
   texture.wrapS = THREE.RepeatWrapping;
@@ -66,7 +30,7 @@ function SphereComponent({ positions }: { positions: [number, number, number] })
     </group>
   );
 }
-function BackgroundStars() {
+function Starparticles() {
   const sceneRef = useRef<Mesh | null>(null);
 
   useFrame(() => {
@@ -78,9 +42,7 @@ function BackgroundStars() {
 
   return (
     <mesh ref={sceneRef}>
-      {/* <Sky></Sky> */}
-      <Stars factor={6} />
-      {/* 팩터 추가  */}
+      <Stars factor={0.2} radius={0.001} depth={0.6} speed={10} fade count={100} />
     </mesh>
   );
 }
@@ -92,27 +54,10 @@ function App() {
         <Canvas>
           <OrbitControls />
           <ambientLight intensity={0.5} />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <BackgroundStars />
-          <TestDode />
-          <SphereComponent positions={[4, 8, 0]} />
-          <TestSphere />
-
+          <Starparticles />
+          <SphereComponent positions={[0, 0, 0]} />
+          <SphereComponent positions={[5, 2, 5]} />
+          <SphereComponent positions={[10, 10, 10]} />
         </Canvas>
       </div>
     </div>
